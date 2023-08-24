00fae324e9a4c9d6a4a1d6f1f015499c3db91697
undrwolf <74624168+undrwolf@users.noreply.github.com>
2023-08-24 23:58:26 +0200

    styling text output with rich module

--- fnds_math/src/fnds_math/main.py
+++ fnds_math/src/fnds_math/main.py
@@ -1,6 +1,8 @@
 import os
 from rich import print as p
 from rich.console import Console
+from rich import pretty
+pretty.install()
 from rich.traceback import install
 install()
 from addition import RandomABIntUpdater
@@ -10,25 +12,29 @@ updater = RandomABIntUpdater()
 def main():
     response = "run"
     score = 0
+    console = Console()
+
     while response != "q":
         correct, a, b = updater.update()
 
         os.system('clear')
-        p(f"score = {score}\n\nWhat is the sum of\n\n{a} + {b} = ?\n\nquit = q\n")
+        console.print(f"score = [bold magenta]{score}[/bold magenta]\n")
+        console.print(f"What is the sum of\n\n[bold]{a} + {b} = ?[/bold]\n")
+        console.print("quit = [bold red]q[/bold red]\n")
 
         response = input()
         if response == "q":
             os.system('clear')
-            p(f"Your score is {score}")
+            console.print(f"Your score is [bold green]{score}[/bold green]")
             break
 
         if int(response) == correct:
             os.system('clear')
-            p('correct')
+            console.print("[bold green]correct[/bold green]")
             score += 1
         else:
             os.system('clear')
-            p('try again')
+            console.print("[bold red]try again[/bold red]")
 
 if __name__ == "__main__":
     main()
