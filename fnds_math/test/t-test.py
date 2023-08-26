8e57579577858491aa9eda7e1f549393b0420a81
undrwolf <74624168+undrwolf@users.noreply.github.com>
2023-08-26 21:30:52 +0200

    rescue the tyed wahlsP

--- fnds_math/test/t-test.py
+++ fnds_math/test/t-test.py
@@ -1,17 +1,25 @@
-from textual.app import App, ComposeResult
-from textual.widgets import Label, Button
 from textual import events
+from textual.app import App, ComposeResult
+from textual.widgets import Label, Button, Header, RichLog
 
 class Fnds_math(App[str]):
     CSS_PATH = "math_app.css"
-    
+
     def on_mount(self) -> None:
         self.screen.styles.background = 'gray'
-    
+
+    TITLE = "A Question App"
+    SUB_TITLE = "The most important question"
+
     def compose(self) -> ComposeResult:
-        yield Label("Do you love Textual?")
+        yield RichLog()
+        yield Header()
+        yield Label("Do you love Textual?", id="question")
         yield Button("Yes", id="yes",  variant="primary")
         yield Button("No", id="no", variant="error")
+
+    def on_key(self, event: events.Key) -> None:
+        self.query_one(RichLog).write(event) 
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         self.exit(event.button.id)
@@ -20,4 +28,4 @@ class Fnds_math(App[str]):
 if __name__ == '__main__':
     app = Fnds_math()
     reply = app.run()
-print(reply)
+    print(reply)
