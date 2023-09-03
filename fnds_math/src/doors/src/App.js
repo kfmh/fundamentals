87fe583ab309873cf4543679e8fd676716d54cd1
undrwolf <74624168+undrwolf@users.noreply.github.com>
2023-09-03 23:57:46 +0200

    yarn recat app test for challange 2

--- fnds_math/src/doors/src/App.js
+++ fnds_math/src/doors/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function LandingPage() {
+  return <h1>Welcome to the Landing Page!</h1>;
+}
+
+function TestPage() {
+  return <h1>This is the Test Page</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Landing Page</Link>
+            </li>
+            <li>
+              <Link to="/test">Test Page</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/test" component={TestPage} />
+      </div>
+    </Router>
   );
 }
 
